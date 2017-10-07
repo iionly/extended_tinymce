b5ebc3ba4cc6442b99e603b2effb22fdbb28da7e
dragonjon <jonaltuna@msn.com>
2017-10-07 17:41:04 +0200

    Spanish translation

---
+++ languages/es.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This file was created by Translation Editor v6.0
+ * On 2017-10-07 17:38
+ */
+
+return array (
+  'extended_tinymce:lang_notice' => 'El idioma de su sitio es %s pero no está instalado para TinyMCE. Consígalo <a target="_blank" href="%s">aquí</a> y cópielo en %s. A continuación, <a href="%s">limpiar las caches</a>. Consulte el archivo README de TinyMCE para obtener más detalles.',
+);
